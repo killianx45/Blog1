2d4598cfb15588e5f3e5a31dd1fe846a425aeccb
Killian <killianjeuffroy@gmail.com>
2025-02-20 15:02:17 +0100

    installation api platform + test api sans token

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use ApiPlatform\Metadata\ApiResource;
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+#[ApiResource]
 class User extends Authenticatable
 {
     use CrudTrait;
